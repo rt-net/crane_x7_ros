ea1d205ea77f4a49c24391b7aabee3e49e38ac2a
Kuwagata <kuwagata@rt-net.jp>
2022-12-13 19:38:18 +0900

    マーカ位置姿勢をtfに配信

--- crane_x7_examples/src/aruco_detection.cpp
+++ crane_x7_examples/src/aruco_detection.cpp
@@ -1,11 +1,14 @@
 #include <cmath>
 #include "rclcpp/rclcpp.hpp"
-#include "rclcpp/wait_for_message.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "opencv2/opencv.hpp"
 #include "opencv2/aruco.hpp"
 #include "cv_bridge/cv_bridge.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+#include "tf2_ros/transform_broadcaster.h"
 using std::placeholders::_1;
 
 class ImageSubscriber : public rclcpp::Node
@@ -19,6 +22,9 @@ class ImageSubscriber : public rclcpp::Node
 
       camera_info_subscription_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(
       "/camera/color/camera_info", 10, std::bind(&ImageSubscriber::camera_info_callback, this, _1));
+
+      tf_broadcaster_ =
+        std::make_unique<tf2_ros::TransformBroadcaster>(*this);
     }
 
   private:
@@ -27,6 +33,7 @@ class ImageSubscriber : public rclcpp::Node
     cv::Ptr<cv::aruco::Dictionary> dictionary
     = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_50);
     sensor_msgs::msg::CameraInfo::SharedPtr camera_info;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
     void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
     {
@@ -54,6 +61,32 @@ class ImageSubscriber : public rclcpp::Node
           {
             cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
           }
+
+          geometry_msgs::msg::TransformStamped t;
+          t.header.stamp = this->get_clock()->now();
+          t.header.frame_id = "camera_color_optical_frame";
+          t.child_frame_id = "target";
+          t.transform.translation.x = tvecs[0][0];
+          t.transform.translation.y = tvecs[0][1];
+          t.transform.translation.z = tvecs[0][2];
+          tf2::Quaternion q;
+          cv::Mat cameraOri;
+          cv::Rodrigues(rvecs[0], cameraOri);
+          tf2::Matrix3x3 mattt = tf2::Matrix3x3(cameraOri.at<double>(0,0),
+            cameraOri.at<double>(0,1),
+            cameraOri.at<double>(0,2),
+            cameraOri.at<double>(1,0),
+            cameraOri.at<double>(1,1),
+            cameraOri.at<double>(1,2),
+            cameraOri.at<double>(2,0),
+            cameraOri.at<double>(2,1),
+            cameraOri.at<double>(2,2));
+          mattt.getRotation(q);
+          t.transform.rotation.x = q.x();
+          t.transform.rotation.y = q.y();
+          t.transform.rotation.z = q.z();
+          t.transform.rotation.w = q.w();
+          tf_broadcaster_->sendTransform(t);
         }
       }
       cv::imshow("out", imageCopy);
