3a722c25f1c81ed2ed0a4bfe632a7afbbe12e439
Kuwagata <kuwagata@rt-net.jp>
2022-12-13 14:18:46 +0900

    マーカ位置をm系に変換

--- crane_x7_examples/src/aruco_detection.cpp
+++ crane_x7_examples/src/aruco_detection.cpp
@@ -1,24 +1,4 @@
-// Copyright 2022 RT Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Reference:
-// https://github.com/ros-planning/moveit2_tutorials/blob
-// /a547cf49ff7d1fe16a93dfe020c6027bcb035b51/doc/move_group_interface
-// /src/move_group_interface_tutorial.cpp
-
 #include <cmath>
-
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/wait_for_message.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
@@ -34,31 +14,56 @@ class ImageSubscriber : public rclcpp::Node
     ImageSubscriber()
     : Node("aruco_detection")
     {
-      subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-      "/camera/color/image_raw", 10, std::bind(&ImageSubscriber::topic_callback, this, _1));
+      image_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+      "/camera/color/image_raw", 10, std::bind(&ImageSubscriber::image_callback, this, _1));
+
+      camera_info_subscription_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(
+      "/camera/color/camera_info", 10, std::bind(&ImageSubscriber::camera_info_callback, this, _1));
     }
 
   private:
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_subscription_;
+    cv::Ptr<cv::aruco::Dictionary> dictionary
+    = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_50);
+    sensor_msgs::msg::CameraInfo::SharedPtr camera_info;
 
-    void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
+    void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
     {
       auto cv_img = cv_bridge::toCvShare(msg, msg->encoding);
       cv::cvtColor(cv_img->image, cv_img->image, cv::COLOR_RGB2BGR);
-
-      cv::Mat image, imageCopy;
+      cv::Mat imageCopy;
       cv_img->image.copyTo(imageCopy);
-      std::vector<int> ids;
-      std::vector<std::vector<cv::Point2f> > corners;
-      cv::aruco::detectMarkers(imageCopy, dictionary, corners, ids);
-      // if at least one marker detected
-      if (ids.size() > 0)
+
+      if(camera_info)
+      {
+        std::vector<int> ids;
+        std::vector<std::vector<cv::Point2f> > corners;
+        cv::aruco::detectMarkers(imageCopy, dictionary, corners, ids);
+        cv::Mat cameraMatrix, distCoeffs;
+        cameraMatrix = cv::Mat(3, 3, CV_64F, camera_info->k.data());
+        distCoeffs = cv::Mat(1, 5, CV_64F, camera_info->d.data());
+
+        if(ids.size() > 0)
+        {
           cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+          std::vector<cv::Vec3d> rvecs, tvecs;
+          cv::aruco::estimatePoseSingleMarkers(
+            corners, 0.04, cameraMatrix, distCoeffs, rvecs, tvecs);
+          for(int i=0; i<int(ids.size()); i++)
+          {
+            cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+          }
+        }
+      }
       cv::imshow("out", imageCopy);
       cv::waitKey(1);
-
     }
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+
+    void camera_info_callback(const sensor_msgs::msg::CameraInfo::SharedPtr msg)
+    {
+      camera_info = msg;
+    }
 };
 
 int main(int argc, char * argv[])
