0ce0997c3f99bf0ddd97465a8342b30d1fc3687b
mizonon <mizoguchi@rt-net.jp>
2024-12-06 14:49:45 +0900

    gz_sim対応

--- crane_x7_gazebo/launch/crane_x7_with_table.launch.py
+++ crane_x7_gazebo/launch/crane_x7_with_table.launch.py
@@ -15,7 +15,8 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from crane_x7_description.robot_description_loader import RobotDescriptionLoader
+from crane_x7_description.robot_description_loader \
+    import RobotDescriptionLoader
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch.actions import IncludeLaunchDescription
@@ -26,22 +27,22 @@ from launch_ros.actions import SetParameter
 
 def generate_launch_description():
     # PATHを追加で通さないとSTLファイルが読み込まれない
-    env = {'IGN_GAZEBO_SYSTEM_PLUGIN_PATH': os.environ['LD_LIBRARY_PATH'],
-           'IGN_GAZEBO_RESOURCE_PATH': os.path.dirname(
+    env = {'GZ_SIM_SYSTEM_PLUGIN_PATH': os.environ['LD_LIBRARY_PATH'],
+           'GZ_SIM_RESOURCE_PATH': os.path.dirname(
                get_package_share_directory('crane_x7_description'))}
     world_file = os.path.join(
         get_package_share_directory('crane_x7_gazebo'), 'worlds', 'table.sdf')
     gui_config = os.path.join(
         get_package_share_directory('crane_x7_gazebo'), 'gui', 'gui.config')
     # -r オプションで起動時にシミュレーションをスタートしないと、コントローラが起動しない
-    ign_gazebo = ExecuteProcess(
-            cmd=['ign gazebo -r', world_file, '--gui-config', gui_config],
+    gz_sim = ExecuteProcess(
+            cmd=['gz sim -r', world_file, '--gui-config', gui_config],
             output='screen',
             additional_env=env,
             shell=True
         )
 
-    ignition_spawn_entity = Node(
+    gazebo_spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
         output='screen',
@@ -54,7 +55,8 @@ def generate_launch_description():
     description_loader = RobotDescriptionLoader()
     description_loader.use_gazebo = 'true'
     description_loader.gz_control_config_package = 'crane_x7_control'
-    description_loader.gz_control_config_file_path = 'config/crane_x7_controllers.yaml'
+    description_loader.gz_control_config_file_path = \
+        'config/crane_x7_controllers.yaml'
     description = description_loader.load()
 
     move_group = IncludeLaunchDescription(
@@ -65,19 +67,28 @@ def generate_launch_description():
         )
 
     spawn_joint_state_controller = ExecuteProcess(
-                cmd=['ros2 run controller_manager spawner joint_state_controller'],
+                cmd=[
+                    'ros2 run controller_manager spawner '
+                    'joint_state_controller'
+                ],
                 shell=True,
                 output='screen',
             )
 
     spawn_arm_controller = ExecuteProcess(
-                cmd=['ros2 run controller_manager spawner crane_x7_arm_controller'],
+                cmd=[
+                    'ros2 run controller_manager spawner '
+                    'crane_x7_arm_controller'
+                ],
                 shell=True,
                 output='screen',
             )
 
     spawn_gripper_controller = ExecuteProcess(
-                cmd=['ros2 run controller_manager spawner crane_x7_gripper_controller'],
+                cmd=[
+                    'ros2 run controller_manager spawner '
+                    'crane_x7_gripper_controller'
+                ],
                 shell=True,
                 output='screen',
             )
@@ -85,14 +96,16 @@ def generate_launch_description():
     bridge = Node(
                 package='ros_gz_bridge',
                 executable='parameter_bridge',
-                arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
+                arguments=[
+                    '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+                ],
                 output='screen'
             )
 
     return LaunchDescription([
         SetParameter(name='use_sim_time', value=True),
-        ign_gazebo,
-        ignition_spawn_entity,
+        gz_sim,
+        gazebo_spawn_entity,
         move_group,
         spawn_joint_state_controller,
         spawn_arm_controller,
