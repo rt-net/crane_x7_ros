7965404711c405fb40de8ac6cb565b9e8fa0bab7
Atsushi Kuwagata <kuwagata@rt-net.jp>
2024-12-26 15:47:03 +0900

    不要な記述を削除

--- crane_x7_moveit_config/launch/run_move_group.launch.py
+++ crane_x7_moveit_config/launch/run_move_group.launch.py
@@ -14,72 +14,31 @@
 
 import os
 
-from ament_index_python.packages import get_package_share_directory
+from crane_x7_description.robot_description_loader import RobotDescriptionLoader
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launch_utils import DeclareBooleanLaunchArg
 from moveit_configs_utils.launches import generate_move_group_launch
 from moveit_configs_utils.launches import generate_moveit_rviz_launch
-from moveit_configs_utils.launches   \
-    import generate_static_virtual_joint_tfs_launch
+from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
 from moveit_configs_utils.launches import generate_rsp_launch
-import yaml
-
-
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return file.read()
-    except (
-        EnvironmentError
-    ):  # parent of IOError, OSError *and* WindowsError where available
-        return None
-
-
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return yaml.safe_load(file)
-    except (
-        EnvironmentError
-    ):  # parent of IOError, OSError *and* WindowsError where available
-        return None
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    declare_loaded_description = DeclareLaunchArgument(
-        'loaded_description',
-        default_value='',
-        description='Set robot_description text.  \
-                     It is recommended to use RobotDescriptionLoader() in  \
-                        crane_x7_description.',
-    )
-
-    ld.add_action(declare_loaded_description)
-
-    ld.add_action(DeclareBooleanLaunchArg('debug', default_value=False))
+    description_loader = RobotDescriptionLoader()
 
     ld.add_action(
         DeclareLaunchArgument(
-            'rviz_config',
-            default_value=get_package_share_directory('crane_x7_moveit_config')
-            + '/config/moveit.rviz',
-            description='Set the path to rviz configuration file.',
+            'loaded_description',
+            default_value=description_loader.load(),
+            description='Set robot_description text.  \
+                        It is recommended to use RobotDescriptionLoader() in  \
+                            crane_x7_description.',
         )
     )
-
-    rviz_config = LaunchConfiguration('rviz_config')
 
     moveit_config = (
         MoveItConfigsBuilder('crane_x7')
@@ -87,24 +46,7 @@ def generate_launch_description():
             publish_robot_description=True,
             publish_robot_description_semantic=True,
         )
-        .robot_description(
-            file_path=os.path.join(
-                get_package_share_directory('crane_x7_description'),
-                'urdf',
-                'crane_x7.urdf.xacro',
-            ),
-            mappings={},
-        )
-        .robot_description_semantic(
-            file_path='config/crane_x7.srdf',
-            mappings={'model': 'crane_x7'},
-        )
-        .joint_limits(file_path='config/joint_limits.yaml')
-        .trajectory_execution(
-            file_path='config/controllers.yaml', moveit_manage_controllers=True
-        )
         .planning_pipelines(pipelines=['ompl'])
-        .robot_description_kinematics(file_path='config/kinematics.yaml')
         .to_moveit_configs()
     )
 
@@ -120,11 +62,7 @@ def generate_launch_description():
     ld.add_entity(generate_move_group_launch(moveit_config))
 
     # RViz
-    rviz_entities = generate_moveit_rviz_launch(moveit_config).entities
-    for entity in rviz_entities:
-        if isinstance(entity, Node):
-            entity.cmd.extend(['--ros-args', '--params-file', rviz_config])
-            ld.add_entity(entity)
+    ld.add_entity(generate_moveit_rviz_launch(moveit_config))
 
     # Static TF
     ld.add_entity(generate_static_virtual_joint_tfs_launch(moveit_config))
