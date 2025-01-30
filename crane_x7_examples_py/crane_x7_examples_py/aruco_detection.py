83703a9d83cc60b30fc69521f43bbe30d7413fb9
Atsushi Kuwagata <kuwagata@rt-net.jp>
2025-01-30 20:46:14 +0900

    マーカの検出失敗時の処理を追加

--- crane_x7_examples_py/crane_x7_examples_py/aruco_detection.py
+++ crane_x7_examples_py/crane_x7_examples_py/aruco_detection.py
@@ -33,7 +33,7 @@ class ImageSubscriber(Node):
         self.camera_info_subscription = self.create_subscription(
             CameraInfo, '/camera/color/camera_info', self.camera_info_callback, 10
         )
-        self.tf_broadcaster = tf2_ros.TransformBroadcaster()
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
 
         self.camera_info = None
         self.bridge = CvBridge()
@@ -53,35 +53,36 @@ class ImageSubscriber(Node):
             corners = []
             # マーカの検出
             corners, ids, _ = aruco.detectMarkers(cv_img, MARKER_DICT)
+            if ids is None:
+                return
             # マーカの検出数
             n_markers = len(ids)
             # カメラパラメータ
-            CAMERA_MATRIX = np.array(self.camera_info['k']).reshape(3, 3)
-            DIST_COEFFS = np.array(self.camera_info['d']).reshape(1, 5)
+            CAMERA_MATRIX = np.array(self.camera_info.k).reshape(3, 3)
+            DIST_COEFFS = np.array(self.camera_info.d).reshape(1, 5)
             # マーカ一辺の長さ 0.04 [m]
             MARKER_LENGTH = 0.04
+            # 画像座標系上のマーカ位置を三次元のカメラ座標系に変換
+            rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(
+                                corners, MARKER_LENGTH, CAMERA_MATRIX, DIST_COEFFS)
 
-            # マーカが一つ以上検出された場合、マーカの位置姿勢をtfで配信
-            if n_markers > 0:
-                for i in range(n_markers):
-                    # 画像座標系上のマーカ位置を三次元のカメラ座標系に変換
-                    rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(
-                                        corners, MARKER_LENGTH, CAMERA_MATRIX, DIST_COEFFS)
-                    # tfの配信
-                    t = TransformStamped()
-                    t.header = msg.header
-                    t.child_frame_id = 'target_' + str(ids[i][0])
-                    t.transform.translation.x = tvecs[i][0][0]
-                    t.transform.translation.y = tvecs[i][0][1]
-                    t.transform.translation.z = tvecs[i][0][2]
-                    # 回転ベクトルをクォータニオンに変換
-                    marker_orientation_rot = Rotation.from_rotvec(rvecs)
-                    marker_orientation_quat = marker_orientation_rot.as_quat()
-                    t.transform.rotation.x = marker_orientation_quat[0]
-                    t.transform.rotation.y = marker_orientation_quat[1]
-                    t.transform.rotation.z = marker_orientation_quat[2]
-                    t.transform.rotation.w = marker_orientation_quat[3]
-                    self.tf_broadcaster.sendTransform(t)
+            # マーカの位置姿勢をtfで配信
+            for i in range(n_markers):
+                # tfの配信
+                t = TransformStamped()
+                t.header = msg.header
+                t.child_frame_id = 'target_' + str(ids[i][0])
+                t.transform.translation.x = tvecs[i][0][0]
+                t.transform.translation.y = tvecs[i][0][1]
+                t.transform.translation.z = tvecs[i][0][2]
+                # 回転ベクトルをクォータニオンに変換
+                marker_orientation_rot = Rotation.from_rotvec(rvecs[i][0])
+                marker_orientation_quat = marker_orientation_rot.as_quat()
+                t.transform.rotation.x = marker_orientation_quat[0]
+                t.transform.rotation.y = marker_orientation_quat[1]
+                t.transform.rotation.z = marker_orientation_quat[2]
+                t.transform.rotation.w = marker_orientation_quat[3]
+                self.tf_broadcaster.sendTransform(t)
 
     def camera_info_callback(self, msg):
         self.camera_info = msg
