668857813ed8fcdc8b97cab922d46d7623aada3d
YusukeKato <yusuke.kato@rt-net.jp>
2025-05-12 11:19:24 +0900

    ros2_controlのコントローラの起動方法をNode()に変更

--- crane_x7_control/launch/crane_x7_control.launch.py
+++ crane_x7_control/launch/crane_x7_control.launch.py
@@ -18,7 +18,6 @@ from ament_index_python.packages import get_package_share_directory
 from crane_x7_description.robot_description_loader import RobotDescriptionLoader
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -64,23 +63,23 @@ def generate_launch_description():
                     crane_x7_controllers],
         )
 
-    spawn_joint_state_controller = ExecuteProcess(
-                cmd=['ros2 run controller_manager spawner joint_state_controller'],
-                shell=True,
-                output='screen',
-            )
+    spawn_joint_state_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['joint_state_broadcaster'])
 
-    spawn_arm_controller = ExecuteProcess(
-                cmd=['ros2 run controller_manager spawner crane_x7_arm_controller'],
-                shell=True,
-                output='screen',
-            )
+    spawn_arm_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['crane_x7_arm_controller'])
 
-    spawn_gripper_controller = ExecuteProcess(
-                cmd=['ros2 run controller_manager spawner crane_x7_gripper_controller'],
-                shell=True,
-                output='screen',
-            )
+    spawn_gripper_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['crane_x7_gripper_controller'])
 
     return LaunchDescription([
       declare_loaded_description,
--- crane_x7_gazebo/launch/crane_x7_with_table.launch.py
+++ crane_x7_gazebo/launch/crane_x7_with_table.launch.py
@@ -64,32 +64,23 @@ def generate_launch_description():
             launch_arguments={'loaded_description': description}.items()
         )
 
-    spawn_joint_state_controller = ExecuteProcess(
-                cmd=[
-                    'ros2 run controller_manager spawner '
-                    'joint_state_controller'
-                ],
-                shell=True,
-                output='screen',
-            )
+    spawn_joint_state_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['joint_state_broadcaster'])
 
-    spawn_arm_controller = ExecuteProcess(
-                cmd=[
-                    'ros2 run controller_manager spawner '
-                    'crane_x7_arm_controller'
-                ],
-                shell=True,
-                output='screen',
-            )
+    spawn_arm_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['crane_x7_arm_controller'])
 
-    spawn_gripper_controller = ExecuteProcess(
-                cmd=[
-                    'ros2 run controller_manager spawner '
-                    'crane_x7_gripper_controller'
-                ],
-                shell=True,
-                output='screen',
-            )
+    spawn_gripper_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['crane_x7_gripper_controller'])
 
     bridge = Node(
                 package='ros_gz_bridge',
