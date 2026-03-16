c8e9a569f2b9e4b75365f470bcb4e33bd25ec856
Kazushi Kurasawa <kurasawa@rt-net.jp>
2026-03-16 15:31:37 +0900

    パラメータ設定の変更に併せ、整合性がとれなくなるrobot_descriptionの指定を削除

--- crane_x7_examples_py/launch/example.launch.py
+++ crane_x7_examples_py/launch/example.launch.py
@@ -38,13 +38,6 @@ def generate_launch_description():
     )
 
     description_loader = RobotDescriptionLoader()
-    declare_loaded_description = DeclareLaunchArgument(
-        'loaded_description',
-        default_value=description_loader.load(),
-        description='Set robot_description text.  \
-                    It is recommended to use RobotDescriptionLoader() \
-                        in crane_x7_description.',
-    )
 
     moveit_config = (
         MoveItConfigsBuilder('crane_x7')
@@ -56,7 +49,7 @@ def generate_launch_description():
     )
 
     moveit_config.robot_description = {
-        'robot_description': LaunchConfiguration('loaded_description')
+        'robot_description': description_loader.load()
     }
 
     # 下記Issue対応のためここでパラメータを設定する
@@ -74,7 +67,6 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            declare_loaded_description,
             declare_example_name,
             declare_use_sim_time,
             example_node,
